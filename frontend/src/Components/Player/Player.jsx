29b979669b9396aa58e6a49a370602e44171c4d4
joao <joaokosour@gmail.com>
2022-07-12 21:57:00 -0300

    made the layout better and buttons reset after each round

--- frontend/src/Components/Player/Player.jsx
+++ frontend/src/Components/Player/Player.jsx
@@ -5,68 +5,92 @@ let numberOfRounds = 3;
 let audio = new Audio();
 
 const handlePlayer = (audio, playing) => {
-  if (playing) audio.play()
-  else audio.pause()
-}
+    if (playing) audio.play();
+    else audio.pause();
+};
 
 const Player = () => {
-  let answer = useRef();
-  let answers = useRef();
-  let [roundNumber, setRoundNumber] = useState(numberOfRounds);
-  const [guesses, setGuesses] = useState([]);
-  const [playlist, setPlaylist] = useState([]);
+    let answer = useRef();
+    let answers = useRef();
+    let [roundNumber, setRoundNumber] = useState(numberOfRounds);
+    const [guesses, setGuesses] = useState([]);
+    const [playlist, setPlaylist] = useState([]);
 
-  const getPlaylist = () => {
-    setPlaylist(mocked_tracks);
-    return;
-    fetch("http://localhost:5000/playlist", { credentials: "include" })
-      .then((response) => response.json())
-      .then((body) => {
-        setPlaylist(body);
-      });
-  };
+    const getPlaylist = () => {
+        setPlaylist(mocked_tracks);
+        return;
+        // fetch("http://localhost:5000/playlist", { credentials: "include" })
+        //     .then((response) => response.json())
+        //     .then((body) => {
+        //         setPlaylist(body);
+        //     });
+    };
 
-  const getResult = (e) => {
-    if (e.target.innerText === answer.current.title)
-      e.target.style.backgroundColor = "green";
-    else e.target.style.backgroundColor = "red";
-    handlePlayer(audio, false);
-    setRoundNumber(roundNumber - 1);
-  };
+    const getResult = async (e) => {
+        if (e.target.innerText === answer.current.title)
+            e.target.classList.add("rightAnswer");
+        else e.target.classList.add("wrongAnswer");
+        handlePlayer(audio, false);
+        await sleep(1000);
+        setRoundNumber(roundNumber - 1);
+    };
 
-  useEffect(() => {
-    if (playlist.length < 1 || roundNumber <= 0) return;
-    answers.current = playlist.splice(0, roundNumber);
-    answer.current = answers.current.pop();
-    const round = playlist.splice(0, 5);
-    round.push(answer.current);
-    round.sort(() => Math.random() - 0.5);
-    setGuesses(round);
-    console.log("Round: " + roundNumber);
-    console.log("Current song: " + answer.current.title)
-    audio = new Audio(answer.current.audio);
-    handlePlayer(audio, true);
-  }, [playlist, roundNumber]);
+    function sleep(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
 
-  return (
-    <div className="player">
-      <p style={{color:"white"}}>{roundNumber}</p>
-      {guesses.length === 0 ? (
-        <div>
-          <button onClick={getPlaylist}>Start</button>
+    useEffect(() => {
+        if (playlist.length < 1 || roundNumber <= 0) return;
+        console.log("1");
+        document.querySelectorAll(".guessButton").forEach((e) => {
+            e.classList.remove("wrongAnswer");
+            e.classList.remove("rightAnswer");
+        });
+        answers.current = playlist.splice(0, roundNumber);
+        answer.current = answers.current.pop();
+        const round = playlist.splice(0, 5);
+        round.push(answer.current);
+        round.sort(() => Math.random() - 0.5);
+        setGuesses(round);
+        console.log("Round: " + roundNumber);
+        console.log("Current song: " + answer.current.title);
+        audio = new Audio(answer.current.audio);
+        handlePlayer(audio, true);
+    }, [playlist, roundNumber]);
+
+    return (
+        <div className='player'>
+            {guesses.length === 0 ? (
+                <div>
+                    <button onClick={getPlaylist}>Start</button>
+                </div>
+            ) : (
+                <>
+                    <p class='roundNumber'>{4 - roundNumber}</p>
+                    <div className='button-container'>
+                        <button class='guessButton' onClick={getResult}>
+                            {guesses[0].title}
+                        </button>
+                        <button class='guessButton' onClick={getResult}>
+                            {guesses[1].title}
+                        </button>
+                        <button class='guessButton' onClick={getResult}>
+                            {guesses[2].title}
+                        </button>
+                        <button class='guessButton' onClick={getResult}>
+                            {guesses[3].title}
+                        </button>
+                        <button class='guessButton' onClick={getResult}>
+                            {guesses[4].title}
+                        </button>
+                        <button class='guessButton' onClick={getResult}>
+                            {guesses[5].title}
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
-      ) : (
-        <div>
-          <button onClick={getResult}>{guesses[0].title}</button>
-          <button onClick={getResult}>{guesses[1].title}</button>
-          <button onClick={getResult}>{guesses[2].title}</button>
-          <button onClick={getResult}>{guesses[3].title}</button>
-          <button onClick={getResult}>{guesses[4].title}</button>
-          <button onClick={getResult}>{guesses[5].title}</button>
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Player;
